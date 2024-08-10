330892a01099dbc3c1cc7cef2aa430f89a5ee449
zhushen <2270364052@qq.com>
2024-08-10 01:09:22 +0000

    new file:   .env 	modified:   app.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+import os
 from flask import Flask, request, jsonify,render_template
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from urllib.parse import quote_plus
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)  # 启用CORS以允许从不同域的前端发送请求
+# 加载 .env 文件中的环境变量
+load_dotenv()
+db_username = os.getenv('DB_USERNAME')
+db_password = quote_plus(os.getenv('DB_PASSWORD'))
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
 
-# 配置数据库
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Zhushen@01@120.79.81.153/ai_tony'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_username}:{db_password}@{db_host}/{db_name}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
